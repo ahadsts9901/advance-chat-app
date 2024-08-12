cf6bc56799422125e252ad6215bec81faaa88e81
ahadsts9901 <ahadsts990@gmail.com>
2024-08-12 17:02:14 +0500

    contacts-section

--- frontend/src/dummy-data.ts
+++ frontend/src/dummy-data.ts
@@ -2,136 +2,151 @@ export const dummyContacts = [
     {
         profilePhoto: "",
         lastMessage: "Hello man",
-        isRead: true,
+        status: "read",
         messageType: "text",
         userName: "Thomas Edison",
-        lastActive: "August 3, 2024, 04:00 PM",
-        _id: ""
+        time: "August 12, 2024, 04:50 PM",
+        _id: "",
+        isRecieved: false
     },
     {
         profilePhoto: "",
         lastMessage: "",
-        isRead: true,
+        status: "delievered",
         messageType: "image",
         userName: "Junaid Akram",
-        lastActive: "August 6, 2024, 02:00 PM",
-        _id: ""
+        time: "August 6, 2024, 02:00 PM",
+        _id: "",
+        isRecieved: true
     },
     {
         profilePhoto: "",
         lastMessage: "",
-        isRead: true,
+        status: "sent",
         messageType: "video",
         userName: "Imran Younus",
-        lastActive: "August 11, 2024, 08:00 AM",
-        _id: ""
+        time: "August 11, 2024, 08:00 AM",
+        _id: "",
+        isRecieved: false
     },
     {
         profilePhoto: "",
         lastMessage: "Hello man",
-        isRead: true,
+        status: "delievered",
         messageType: "text",
         userName: "Thomas Edison",
-        lastActive: "August 3, 2024, 04:00 PM",
-        _id: ""
+        time: "August 3, 2024, 04:00 PM",
+        _id: "",
+        isRecieved: false
     },
     {
         profilePhoto: "",
         lastMessage: "",
-        isRead: true,
+        status: "delievered",
         messageType: "image",
         userName: "Junaid Akram",
-        lastActive: "August 6, 2024, 02:00 PM",
-        _id: ""
+        time: "August 6, 2024, 02:00 PM",
+        _id: "",
+        isRecieved: true
     },
     {
         profilePhoto: "",
         lastMessage: "",
-        isRead: true,
+        status: "read",
         messageType: "video",
         userName: "Imran Younus",
-        lastActive: "August 11, 2024, 08:00 AM",
-        _id: ""
+        time: "August 11, 2024, 08:00 AM",
+        _id: "",
+        isRecieved: true
+    },
+    {
+        profilePhoto: "",
+        lastMessage: "",
+        status: "read",
+        messageType: "audio",
+        userName: "Thomas Edison",
+        time: "August 3, 2024, 04:00 PM",
+        _id: "",
+        isRecieved: true
+    },
+    {
+        profilePhoto: "",
+        lastMessage: "",
+        status: "sent",
+        messageType: "image",
+        userName: "Junaid Akram",
+        time: "August 6, 2024, 02:00 PM",
+        _id: "",
+        isRecieved: true
+    },
+    {
+        profilePhoto: "",
+        lastMessage: "",
+        status: "sent",
+        messageType: "video",
+        userName: "Imran Younus",
+        time: "August 11, 2024, 08:00 AM",
+        _id: "",
+        isRecieved: true
     },
     {
         profilePhoto: "",
         lastMessage: "Hello man",
-        isRead: true,
+        status: "sent",
         messageType: "text",
         userName: "Thomas Edison",
-        lastActive: "August 3, 2024, 04:00 PM",
-        _id: ""
+        time: "August 3, 2024, 04:00 PM",
+        _id: "",
+        isRecieved: true
     },
     {
         profilePhoto: "",
         lastMessage: "",
-        isRead: true,
+        status: "sent",
         messageType: "image",
         userName: "Junaid Akram",
-        lastActive: "August 6, 2024, 02:00 PM",
-        _id: ""
+        time: "August 6, 2024, 02:00 PM",
+        _id: "",
+        isRecieved: true
     },
     {
         profilePhoto: "",
         lastMessage: "",
-        isRead: true,
+        status: "sent",
         messageType: "video",
         userName: "Imran Younus",
-        lastActive: "August 11, 2024, 08:00 AM",
-        _id: ""
+        time: "August 11, 2024, 08:00 AM",
+        _id: "",
+        isRecieved: true
     },
     {
         profilePhoto: "",
         lastMessage: "Hello man",
-        isRead: true,
+        status: "sent",
         messageType: "text",
         userName: "Thomas Edison",
-        lastActive: "August 3, 2024, 04:00 PM",
-        _id: ""
+        time: "August 3, 2024, 04:00 PM",
+        _id: "",
+        isRecieved: true
     },
     {
         profilePhoto: "",
         lastMessage: "",
-        isRead: true,
+        status: "sent",
         messageType: "image",
         userName: "Junaid Akram",
-        lastActive: "August 6, 2024, 02:00 PM",
-        _id: ""
+        time: "August 6, 2024, 02:00 PM",
+        _id: "",
+        isRecieved: true
     },
     {
         profilePhoto: "",
         lastMessage: "",
-        isRead: true,
+        status: "sent",
         messageType: "video",
         userName: "Imran Younus",
-        lastActive: "August 11, 2024, 08:00 AM",
-        _id: ""
-    },
-    {
-        profilePhoto: "",
-        lastMessage: "Hello man",
-        isRead: true,
-        messageType: "text",
-        userName: "Thomas Edison",
-        lastActive: "August 3, 2024, 04:00 PM",
-        _id: ""
-    },
-    {
-        profilePhoto: "",
-        lastMessage: "",
-        isRead: true,
-        messageType: "image",
-        userName: "Junaid Akram",
-        lastActive: "August 6, 2024, 02:00 PM",
-        _id: ""
-    },
-    {
-        profilePhoto: "",
-        lastMessage: "",
-        isRead: true,
-        messageType: "video",
-        userName: "Imran Younus",
-        lastActive: "August 11, 2024, 08:00 AM",
-        _id: ""
+        time: "August 11, 2024, 08:00 AM",
+        _id: "",
+        isRecieved: true
     },
 ]
--- frontend/src/pages/home/components/chat/SingleContact.tsx
+++ frontend/src/pages/home/components/chat/SingleContact.tsx
@@ -1,7 +1,66 @@
 import "./main.css"
 import fallBackProfileImage from "/default_avatar.png"
+import DoneIcon from '@mui/icons-material/Done';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import { FaCamera } from "react-icons/fa";
+import { FaVideo } from "react-icons/fa6";
+import { FaMicrophone } from "react-icons/fa";
+import moment from "moment"
+
+const Status = ({ status }: any) => {
+
+    return (
+        <>
+            <div className="readReciepts">
+                {status === "sent" && <DoneIcon className="sent" />}
+                {status === "delievered" && <DoneAllIcon className="delievered" />}
+                {status === "read" && <DoneAllIcon className="read" />}
+            </div>
+        </>
+    )
+
+}
+
+const MessageType = ({ lastMessage, messageType }: any) => {
+
+    return (
+        <>
+            <div className="messageType">
+                {messageType === "text" && lastMessage && <p>{lastMessage?.length > 15 ? `${lastMessage?.substr(0, 20)}...` : lastMessage}</p>}
+                {messageType === "image" && <><FaCamera style={{ marginRight: "2px" }} /><p>Photo</p></>}
+                {messageType === "video" && <><FaVideo style={{ marginRight: "2px" }} /><p>Video</p></>}
+                {messageType === "audio" && <><FaMicrophone style={{ marginRight: "2px" }} /><p>Audio</p></>}
+            </div>
+        </>
+    )
+
+}
 
 const SingleContact = ({ data }: any) => {
+
+    const timeAgo = (date: string) => {
+        const now = moment();
+        const momentDate = moment(date);
+        const diffInHours = now.diff(momentDate, 'hours');
+
+        if (diffInHours < 24) {
+            return momentDate.format('h:mm A');
+        } else {
+            const diffInDays = now.diff(momentDate, 'days');
+            if (diffInDays < 30) {
+                return diffInDays === 1 ? '1 day ago' : `${diffInDays} days ago`;
+            } else {
+                const diffInMonths = now.diff(momentDate, 'months');
+                if (diffInMonths < 12) {
+                    return diffInMonths === 1 ? '1 month ago' : `${diffInMonths} months ago`;
+                } else {
+                    const diffInYears = now.diff(momentDate, 'years');
+                    return diffInYears === 1 ? '1 year ago' : `${diffInYears} years ago`;
+                }
+            }
+        }
+    };
+
     return (
         <>
             <div className="singleContact">
@@ -12,6 +71,14 @@ const SingleContact = ({ data }: any) => {
                             e.target.style.padding = "0.4em"
                         }}
                     />
+                    <div>
+                        <h4>{data?.userName}</h4>
+                        <div>
+                            {!data?.isRecieved ? <Status status={data?.status} /> : null}
+                            <MessageType lastMessage={data?.lastMessage} messageType={data?.messageType} />
+                        </div>
+                    </div>
+                    <p className="messageTime">{timeAgo(data?.time)}</p>
                 </div>
             </div>
         </>
