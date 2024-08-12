2c3a4c803ab34885ee60791118438a11b886f506
ahadsts9901 <ahadsts990@gmail.com>
2024-08-12 13:26:12 +0500

    chat

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,9 +1,14 @@
-// import "./Home.css"
+import Chats from "./components/Chats"
+import Conversation from "./components/Conversation"
+import "./Home.css"
 
 const Home = () => {
   return (
     <>
-    
+      <div className="homePageBar">
+        <Chats />
+        <Conversation />
+      </div>
     </>
   )
 }
