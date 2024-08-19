d4dee3b5de74a873899ac7fc83475c743a08601e
ahadsts9901 <ahadsts990@gmail.com>
2024-08-19 17:54:22 +0500

    create-message

--- backend/routes/chatRoutes.mjs
+++ backend/routes/chatRoutes.mjs
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createMessageController, getAllContactsWithChatsController } from "../controllers/chatControllers.mjs";
+import { upload } from "../utils/multer.mjs"
 
 const router = Router()
 
 router.get("/contacts", getAllContactsWithChatsController)
 
-router.post("/chats/:to_id", createMessageController)
+router.post("/chats/:to_id", upload?.any(), createMessageController)
 
 export default router
