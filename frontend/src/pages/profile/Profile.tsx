f48fd22fdbce6cbaaec9541d332128d88cd94a1e
ahadsts9901 <ahadsts990@gmail.com>
2024-09-04 09:57:16 -0700

    got-user-data

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -1,14 +1,48 @@
-import { useParams } from "react-router-dom"
 import "./Main.css"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { baseUrl } from "../../core"
 
 const Profile = () => {
 
     const { userId } = useParams()
 
+    const [user, setUser] = useState<any>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+
+        getProfile()
+
+    }, [userId])
+
+    const getProfile = async () => {
+
+        if (!userId || userId?.trim() === "") return
+
+        try {
+
+            setIsLoading(true)
+
+            const resp = await axios.get(`${baseUrl}/api/v1/profile/${userId}`, { withCredentials: true })
+
+            setIsLoading(false)
+            setUser(resp?.data?.data)
+
+        } catch (error) {
+            console.error(error)
+            setIsLoading(false)
+        }
+
+    }
+
     return (
         <>
             <div>
                 Profile : {userId}
+                <br />
+                {isLoading ? "loading..." : JSON.stringify(user)}
             </div>
         </>
     )
