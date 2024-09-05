325f3088354d60846e0b89142f49f72f7ce988ce
ahadsts9901 <ahadsts990@gmail.com>
2024-09-05 17:25:47 +0500

    profile-section-completed

--- frontend/src/pages/profile/components/ProfileSection.tsx
+++ frontend/src/pages/profile/components/ProfileSection.tsx
@@ -1,16 +1,54 @@
-import DataSection from "./DataSection"
-import "./Main.css"
-import MediaSection from "./MediaSection"
+import "./Main.css";
+import DataSection from "./DataSection";
+import MediaSection from "./MediaSection";
+import { IoMdShare } from "react-icons/io";
+import { MdChat } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { MdOutlineDownloadDone } from "react-icons/md";
+
+const OptProfile = ({ data }: any) => {
+    return (
+        <div className="profile-option" onClick={data?.fun}>
+            {data?.icon}
+            <p>{data?.label}</p>
+        </div>
+    );
+};
 
 const ProfileSection = ({ user }: any) => {
-    return (
-        <>
-            <div className="profileSection">
-                <DataSection user={user} />
-                <MediaSection user={user} />
-            </div>
-        </>
-    )
-}
 
-export default ProfileSection
+    const navigate = useNavigate();
+
+    const [copyLabel, setCopyLabel] = useState("Share Profile");
+    const [copyIcon, setCopyIcon] = useState(<IoMdShare />)
+
+    const chatFunction = () => navigate(`/chat/${user?._id}`);
+
+    const copyFun = () => {
+        navigator.clipboard.writeText(`${window?.location?.host}/profile/${user?._id}`);
+        setCopyLabel("Link Copied!");
+        setCopyIcon(<MdOutlineDownloadDone />)
+        setTimeout(() => {
+            setCopyLabel("Share Profile")
+            setCopyIcon(<IoMdShare />)
+        }, 1000);
+    };
+
+    const options = [
+        { label: copyLabel, path: "profile", icon: copyIcon, fun: copyFun },
+        { label: "Chat", path: "chat", icon: <MdChat />, fun: chatFunction },
+    ];
+
+    return (
+        <div className="profileSection">
+            <DataSection user={user} />
+            <MediaSection user={user} />
+            {options?.map((option: any, i: number) => (
+                <OptProfile key={i} data={option} />
+            ))}
+        </div>
+    );
+};
+
+export default ProfileSection;
