19a53523a996d20bb8c35e261e8fdad77e34c5c7
ahadsts9901 <ahadsts990@gmail.com>
2024-09-10 16:11:43 +0500

    draggable-component

--- frontend/src/components/mui/DraggableBox.tsx
+++ frontend/src/components/mui/DraggableBox.tsx
@@ -1,52 +1,26 @@
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Paper, { PaperProps } from '@mui/material/Paper';
+import { IconButton } from '@mui/material';
+import { RxCross2 } from "react-icons/rx";
 import Draggable from 'react-draggable';
 
-function PaperComponent(props: PaperProps) {
+const DraggableBox = ({ open, setOpen }: any) => {
+    console.log("open", open)
     return (
-        <Draggable
-            handle="#draggable-dialog-title"
-            cancel={'[class*="MuiDialogContent-root"]'}
-        >
-            <Paper {...props} />
+        <Draggable>
+            <div
+                style={{
+                    width: '200px',
+                    height: '200px',
+                    background: 'red',
+                    position: 'fixed',
+                    zIndex: 20,
+                    cursor: 'move'
+                }}
+            >
+                call component
+                <IconButton onClick={() => setOpen(false)}><RxCross2 /></IconButton>
+            </div>
         </Draggable>
     );
-}
+};
 
-export default function DraggableBox({ open, setOpen }: any) {
-
-    const handleClose = () => setOpen(false)
-
-    return (
-        <>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                PaperComponent={PaperComponent}
-                aria-labelledby="draggable-dialog-title"
-            >
-                <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
-                    Subscribe
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We
-                        will send updates occasionally.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button autoFocus onClick={handleClose}>
-                        Cancel
-                    </Button>
-                    <Button onClick={handleClose}>Subscribe</Button>
-                </DialogActions>
-            </Dialog>
-        </>
-    );
-
-}
+export default DraggableBox;
